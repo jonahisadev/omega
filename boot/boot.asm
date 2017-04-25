37f07b2a9db90f75d4021bc2ec4412fcc1da42c7
Jonah <jonahisadev@gmail.com>
2017-04-25 17:37:29 -0500

    Commit #0004 (BIOS disk loading)

--- boot/boot.asm
+++ boot/boot.asm
@@ -1,6 +1,8 @@
 [ORG 0x7C00]
 
 main:
+	mov [BOOT_DRIVE], dl
+
 	mov bp, 0x8000
 	mov sp, bp
 	
@@ -8,14 +10,28 @@ main:
 	call print
 	call print_nl
 	
-	mov dx, 0xFAC3
+	mov dh, 2
+	mov bx, 0x9000
+	mov dl, [BOOT_DRIVE]
+	call disk_load
+	
+	mov dx, [0x9000]
+	call print_hex
+	call print_nl
+	
+	mov dx, [0x9000 + 512]
 	call print_hex
 	
 	jmp $
 	
 %include "boot/print.asm"
+%include "boot/disk.asm"
 
 HELLO: db "Hello BIOS!",0
+BOOT_DRIVE: db 0
 	
 times 510 - ($ - $$) db 0
 dw 0xAA55
+
+times 256 dw 0xACDC
+times 256 dw 0xFACE
